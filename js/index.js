659906d09a4d0ae56765c3d2b1125561aa48b67c
tdmmdos <correo.tdmmdos@gmail.com>
2026-04-08 21:37:27 -0300

    Updatecon fecha nueva index.js

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // Mandar al form
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz2vIlMrxhv7zh-1pZ5K2_fVD33mC2os0K2HmKq32vZYodE8nds82kOqYK6pLoJd14aqQ/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzQwdsdcjKzGUGMJT-wXyShH3hdcDBdMcppxInffYft0MWeKLCCaA00SRcwURmLdACpQg/exec'
 const form = document.forms['my-google-sheet']
 const btnSubmit = document.getElementById('btnSubmit');
 
